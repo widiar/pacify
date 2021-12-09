1a57df0a8d2a178577c2c454bf5a3463d46ea7e9
widiar <awidiarsana.aw@gmail.com>
2021-12-09 20:33:10 +0800

    add article migration

---
+++ resources/views/admin/article/index.blade.php
@@ -0,0 +1,66 @@
+@extends('admin.template.admin')
+
+@section('title', 'Article')
+
+@section('main-content')
+<a href="{{ route('admin.article.create') }}">
+    <button class="btn btn-primary btn-sm mb-3 ml-3">Tambah Artikel</button>
+</a>
+<div class="card shadow mx-3">
+    <div class="card-body table-responsive">
+        @if(session('success'))
+        <div class="alert alert-success alert-dismissible">
+            <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+            <h5><i class="icon fas fa-check"></i> BERHASIL!</h5>
+            {{session('success')}}
+        </div>
+        @elseif(session('error'))
+        <div class="alert alert-danger alert-dismissible">
+            <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+            <h5><i class="icon fas fa-exclamation-triangle"></i> GAGAL!</h5>
+            {{session('error')}}
+        </div>
+        @endif
+        <table id="adminTable" class="table table-bordered dt-responsive nowrap" style="width: 100%;">
+            <thead>
+                <tr>
+                    <th>NO</th>
+                    <th>Title</th>
+                    <th>Tanggal</th>
+                    <th class="text-center">Aksi</th>
+                </tr>
+            </thead>
+            <tbody class="actionz">
+                @php
+                $no=0;
+                @endphp
+                @if (!is_null($data))
+                @foreach ($data as $dt)
+                <tr>
+                    <td>{{ ++$no }}</td>
+                    <td>{{ $dt->title }}</td>
+                    <td class="text-center">
+                        {{ date('d/m/y h:i A', strtotime($dt->updated_at)) }}
+                    </td>
+                    <td class="text-center">
+                        <div class="row" style="min-width: 100px">
+                            <a href="{{ route('admin.article.edit', $dt->id) }}" class="mx-3">
+                                <button class="btn btn-sm btn-primary"><i class="fas fa-edit"></i></button>
+                            </a>
+                            <form action="{{ route('admin.article.destroy', $dt->id) }}" method="POST" class="deleted">
+                                @method("DELETE")
+                                @csrf
+                                <button class="btn btn-sm btn-danger" type="submit"><i
+                                        class="fas fa-trash"></i></button>
+                            </form>
+                        </div>
+                    </td>
+                </tr>
+                @endforeach
+                @endif
+            </tbody>
+        </table>
+    </div>
+</div>
+
+@endsection
