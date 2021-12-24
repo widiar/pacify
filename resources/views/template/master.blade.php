a898bf314d7577775cb84b9934bec6e59ca89aa2
widiar <awidiarsana.aw@gmail.com>
2021-12-24 15:18:45 +0800

    add meta tag

--- resources/views/template/master.blade.php
+++ resources/views/template/master.blade.php
@@ -5,6 +5,8 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="dicoding:email" content="awidiarsana.aw@gmail.com">
+    <meta name="dicoding:email" content="">
     <link href="{{ asset('img/pacify-icon.png') }}" rel="icon">
     <title>@yield('title')</title>
     <link rel="stylesheet" href="{{ asset('plugins/fontawesome-free/css/all.min.css') }}">
